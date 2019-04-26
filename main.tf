c63b83cc91de0020b2e29eb9d0dbcd559a1a8d60
Bastien <bastien.rios@gmail.com>
2019-04-26 14:23:11 +0200

    Second commit

--- main.tf
+++ main.tf
@@ -10,11 +10,10 @@ ressource "aws.aws_vpc" "main" {
 }
 
 resource "aws.aws_subnet" "pub_subnet" {
-  
+    cidr_block  ="10.0.1.0/16"
 }
 
 resource "aws.aws_subnet" "priv_subnet" {
-    
-  
+    cidr_block  ="10.0.2.0/16"
 }
 
